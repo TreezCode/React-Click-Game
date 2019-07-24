3ba11e94705e47b63a4a57fbc71bb9e4c25bc39e
TreezCode <TreezCode@users.noreply.github.com>
2019-07-23 22:57:26 -0600

    images/navbar

---
+++ src/components/Game.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Navbar from "./Navbar";
+
+class Game extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <>
+                <Navbar />
+            </>
+        );
+    }
+}
+
+export default Game;
